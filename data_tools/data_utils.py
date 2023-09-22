53c938c2c5cc1e000ceefc1edf3ac5e953464248
zhengchaofan <zhengcfwork@gmail.com>
2023-09-22 14:54:12 +0800

    Fix(data_analysis):增加sep

--- data_tools/data_utils.py
+++ data_tools/data_utils.py
@@ -15,8 +15,8 @@ exp6	4.39412E-05
 base1	9.08746E-05
 exp6	0.000883182
 """
-def read_csv1(filename, key_column, key_value):
-  file_data = pd.read_csv(filename, sep=",").dropna()
+def read_csv1(filename, key_column, key_value, sep):
+  file_data = pd.read_csv(filename, sep=sep).dropna()
   process_data = []
   for key in key_value:
     process_data.append(file_data[file_data[key_column] == key].dropna())
@@ -32,8 +32,9 @@ def get_data_bins(data, bins_number):
   bins = np.histogram_bin_edges(data, bins=bins_number)
   return bins
 
-def draw_histogram(filename, key_column = 'expname', key_value = ['base1', 'exp6'], data_column = 'cvr', bins_number = 100, is_log=False):
-  process_data = read_csv1(filename, key_column, key_value)
+def draw_histogram(filename, sep=',', key_column = 'expname', 
+  key_value = ['base1', 'exp6'], data_column = 'cvr', bins_number = 100, is_log=False, draw_flag="exp",):
+  process_data = read_csv1(filename, key_column, key_value, sep=sep)
   if is_log:
     for i in range(len(process_data)):
       process_data[i][data_column] = np.log(process_data[i][data_column])
@@ -43,10 +44,10 @@ def draw_histogram(filename, key_column = 'expname', key_value = ['base1', 'exp6
   # exp_bins = get_data_bins(exp_data[data_column], bins_number)
 
   for i, data in enumerate(process_data):
-    plt.hist(data[data_column], bins=bins_number, alpha=0.5, label='{0}_{1}_hist'.format(key_column, key_value[i]))
+    plt.hist(data[data_column], bins=bins_number, alpha=0.5, label='{0}_{1}_hist_{2}'.format(key_column, key_value[i], draw_flag))
 
   plt.xlabel(data_column)
   plt.ylabel('Frequency')
   plt.legend(loc='upper right')
   plt.title('Frequency Distribution of Lps cvr')
-  plt.show()
+  # plt.show()
--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from data_tools.data_utils import *
 #   key_column="expname", key_value=['base1', 'exp6'], data_column='cvr'
 #   ,bins_number=100, is_log=True)
 
-draw_histogram("./data_files/lps_cvr_by_pos.csv", 
-  key_column="pos", key_value=[0,1,2,5,6,7,8], data_column='cvr'
-  ,bins_number=100, is_log=True)
+# draw_histogram("./data_files/lps_cvr_by_pos.csv", sep=',',
+#   key_column="pos", key_value=[0], data_column='cvr'
+#   ,bins_number=100, is_log=True)
+
+key_column="pos"
+key_value=[1]
+data_column='cvr'
+
+draw_histogram("./data_files/lps_cvr_by_pos_exp6.csv", sep='\t',
+  key_column=key_column, key_value=key_value, data_column=data_column,
+  bins_number=100, draw_flag="exp", is_log=True)
+
+draw_histogram("./data_files/lps_cvr_by_pos_base1.csv", sep='\t',
+  key_column=key_column, key_value=key_value, data_column=data_column,
+  bins_number=100, draw_flag="exp", is_log=True)
+
+import matplotlib.pyplot as plt
+plt.show()
