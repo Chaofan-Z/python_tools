03cdbd4d0a30e56ff8d97b25413f7747710babb0
zhengchaofan <zhengcfwork@gmail.com>
2025-07-23 10:16:43 +0800

    Add(test): space line

--- get_bins.py
+++ get_bins.py
@@ -6,6 +6,7 @@ import pandas as pd
 from data_tools.data_utils import get_data_bins
 data = pd.read_csv("./data_files/conversion.csv")
 
+
 print(data.info())
 bins = get_data_bins(data['cvr'], 9)
 print(bins)
